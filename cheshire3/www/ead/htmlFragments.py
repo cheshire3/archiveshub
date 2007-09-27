35f892f16d860ea26a4ac8a65b3cfa75e164dfc7
cheshire <cheshire@04eb1732-f344-4bcc-95d5-7f11b6d85e6b>
2007-09-27 13:42:07 +0000

    Combined JH and CS vesions

--- cheshire3/www/ead/htmlFragments.py
+++ cheshire3/www/ead/htmlFragments.py
@@ -1,18 +1,19 @@
 #
-# Script:     htmlFragments.py
-# Version:    0.02
+# Script:      htmlFragments.py
+# Version:     0.02
+# Date:        25 September 2007
+# Copyright:   &copy; University of Liverpool 2005-2007
 # Description:
 #            HTML fragments used by Cheshire for Archives
 #
-# Language:  Python
-# Author:    John Harrison <john.harrison@liv.ac.uk>
-# Date:      25 September 2007
-#
-# Copyright: &copy; University of Liverpool 2005-2007
+# Author(s): JH - John Harrison <john.harrison@liv.ac.uk>
+#            CS - Catherine Smith <catherine.smith@liv.ac.uk>
 #
 # Version History:
 # 0.01 - 03/01/2006 - JH - HTML Fragments migrated from localConfig.py
-# 0.02 - 26/09/2007 - JH - Mods to component search display to accomodate hierarchy of titles
+# 0.02 - 06/08/2007 - CS - rename_notes added
+# 0.03 - 26/09/2007 - JH - Mods to component search display to accomodate hierarchy of titles
+#                        - Folder tags added
 #
 # NB:
 # - If you are not experieced in editing HTML you are advised not to edit any of the HTML fragments
@@ -24,9 +25,6 @@
 #
 #
 #
-#
-
-
 
 # img tags for icons used in results
 # - N.B. These will only be displayed if the result_graphics switch is set to 1
@@ -149,3 +147,13 @@ new_user_template = '''
   </flags>
 </config>'''
 
+rename_notes = '''
+<div id="notes">
+  <strong>Notes</strong>: 
+  <em>
+    <ul>
+      <li>You should avoid using spaces, accented characters, or punctuation in the filename. e.g. slashes, full-stops etc. Dashes are OK.</li>
+      <li>as a file extension (e.g. \'sgml\', \'xml\') is essential, this will be added automatically. You may choose which of these is appropriate to the file.</li>
+    </ul>
+  </em>
+</div>'''
