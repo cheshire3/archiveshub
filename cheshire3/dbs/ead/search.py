c50dbbcf0325f83e3668eeb89bcadd18af7ae7c4
John Harrison <john.harrison@liv.ac.uk>
2012-05-23 11:59:28 +0100

    made search.py PEP8 compliant

--- cheshire3/dbs/ead/search.py
+++ cheshire3/dbs/ead/search.py
@@ -1,21 +1,18 @@
 #!/home/cheshire/install/bin/python -i
-## Script:    search.py
+#
+# Script:    search.py
 # Date:      11 February 2008
 # Copyright: &copy; University of Liverpool 2005-2008
-# Description:
-#            Quick search testing script
-#            - part of Cheshire for Archives v3
-#
 # Author(s): JH - John Harrison <john.harrison@liv.ac.uk>
-#
 # Language:  Python
 #
+"""Search the Cheshire3 for Archives database of EAD finding aid documents."""
 
-import sys, os, time
+import sys
+import os
+import time
+
 from lxml import etree
-
-sys.path.insert(1, "../../www/ead")
-sys.path.insert(1, "../../code")
 
 from cheshire3.baseObjects import Session
 from cheshire3.server import SimpleServer
@@ -28,11 +25,18 @@ def _backwalkTitles(rec, xpath):
     xpathParts = xpath.split('/')
     while xpathParts[-1] != 'dsc':
         try:
-            tn = rec.process_xpath(session, '/'.join(xpathParts) + '/did/unittitle')[0]
+            tn = rec.process_xpath(
+                               session, 
+                               '/'.join(xpathParts) + '/did/unittitle'
+                               )[0]
             t = flattenTexts(tn)
             titles.append(t.strip())
         except IndexError:
-            print etree.dump(rec.process_xpath(session, '/'.join(xpathParts) + '/did')[0])
+            print etree.dump(rec.process_xpath(
+                                           session, 
+                                           '/'.join(xpathParts) + '/did'
+                                           )[0]
+                             )
             raise
             
         xpathParts.pop(-1)
@@ -40,12 +44,15 @@ def _backwalkTitles(rec, xpath):
     titles.reverse()
     return titles
 
+
 def _backwalkTitles2(rec, xpath):
     node = rec.get_dom(session).xpath(xpath)[0]
     titles = [node.xpath('string(./did/unittitle)')]
     for n in node.iterancestors():
-        if n.tag == 'dsc': continue
-        elif n.tag == 'ead': break
+        if n.tag == 'dsc':
+            continue
+        elif n.tag == 'ead':
+            break
         t = n.xpath('string(./did/unittitle)')
         titles.append(t.strip())
 #        try:
@@ -73,7 +80,8 @@ def doSearch(qString):
             parId = rec.process_xpath(session, '/c3component/@parent')[0]
             parId = parId.split('/')[-1]
         except IndexError:
-            titles = [rec.process_xpath(session, '/*/*/did/unittitle/text()')[0]]
+            titles = [rec.process_xpath(session, 
+                                        '/*/*/did/unittitle/text()')[0]]
         else:
             parRec = recordStore.fetch_record(session, parId)
             xpath = rec.process_xpath(session, '/c3component/@xpath')[0]
@@ -83,7 +91,7 @@ def doSearch(qString):
         for y, t in enumerate(titles):
             if y:
                 print ' ',
-            print (' ' * (y*2) ),t
+            print (' ' * (y * 2)), t
     return rs
 
 # Build environment...
@@ -99,7 +107,8 @@ if len(sys.argv[1:]):
     qString = ' '.join(sys.argv[1:])
 else:
     qString = 'cql.anywhere all/relevant/proxinfo "money"'
-    qString = '((cql.anywhere all/relevant/proxinfo "money") or/relevant/proxinfo (dc.description all/relevant/proxinfo "money") or/relevant/proxinfo (dc.title all/relevant/proxinfo "money"))'
+    clauses = ['(cql.anywhere all/relevant/proxinfo "money")',
+               '(dc.description all/relevant/proxinfo "money")',
+               '(dc.title all/relevant/proxinfo "money")']
+    qString = 'or/relevant/proxinfo'.join(clauses)
 doSearch(qString)
-
-    
