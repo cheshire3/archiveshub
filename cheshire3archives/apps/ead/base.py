4929226794610202ca0402d00add657ddb76b503
John Harrison <john.harrison@liv.ac.uk>
2013-02-15 11:37:25 +0000

    added outline of record resolver WSGI app

--- cheshire3archives/apps/ead/base.py
+++ cheshire3archives/apps/ead/base.py
@@ -1,24 +1,38 @@
 """Abstract Base Class for EAD WSGI Applications."""
 
+import os
 import sys
 import traceback
 
-from cheshire3.web.www_utils import html_encode
 
-# Local imports
-from localConfig import *
+from cheshire3.baseObjects import Session
+import cheshire3.exceptions as c3errors
+from cheshire3.internal import cheshire3Root
+from cheshire3.server import SimpleServer
+from cheshire3.web.www_utils import html_encode
+from ConfigParser import SafeConfigParser
+
 
 class EADWsgiApplication(object):
     
-    def __init__(self):
+    def __init__(self, session, database, config):
+        self.session = session
+        self.database = database
+        self.queryFactory = self.database.get_object(session,
+                                                     'defaultQueryFactory')
+        self.globalReplacements = {
+#            '%REP_NAME%': repository_name,
+#            '%REP_LINK%': repository_link,
+#            '%REP_LOGO%': repository_logo,
+            '<br>': '<br/>',
+            '<hr>': '<hr/>'
+        }
+    
+    def __call__(self, environ, start_response):
+        # Method to make instances of this class callable
+        self.response_headers = []
         self.htmlTitle = []
         self.htmlNav = []
-        self.globalReplacements = {'%REP_NAME%': repository_name,
-                                   '%REP_LINK%': repository_link,
-                                   '%REP_LOGO%': repository_logo,
-                                   '<br>': '<br/>',
-                                   '<hr>': '<hr/>'
-                                   }
 
     def _handle_error(self):
         self.htmlTitle.append('Error')
@@ -32,35 +46,57 @@ class EADWsgiApplication(object):
         # TODO: add logging
         #self.log('*** {0}: {1}'.format(excName, excArgs))
         #self.logExc('{0}: {1}\n{2}'.format(excName, excArgs, '\n'.join(excTb)))
-        excName = html_encode(excName)
-        excArgs = html_encode(excArgs)
-        
-        excTb = '<br/>\n'.join([html_encode(line) for line in excTb])
-        return '''\
-        <div id="single">
-          <p class="error">An error occurred while processing your request.
-            <br/>The message returned was as follows:
-          </p>
-          <code>{0}: {1}</code>
-          <p>
-            <strong>
-              Please try again, or contact the system administrator if this 
-              problem persists.
-            </strong>
-          </p>
-          <p>Debugging Traceback: 
-            <a href="#traceback" class="jstoggle-text">[ hide ]</a>
-          </p>
-          <div id="traceback" class="jshide">{2}</div>
-        </div> <!-- /single -->
-        '''.format(excName, excArgs, excTb).split('\n')
+        yield html_encode(excName)
+        yield html_encode(excArgs)
+        for line in excTb:
+            yield html_encode(line)
+            yield '<br/>\n'
+#        return '''\
+#        <div id="single">
+#          <p class="error">An error occurred while processing your request.
+#            <br/>The message returned was as follows:
+#          </p>
+#          <code>{0}: {1}</code>
+#          <p>
+#            <strong>
+#              Please try again, or contact the system administrator if this 
+#              problem persists.
+#            </strong>
+#          </p>
+#          <p>Debugging Traceback: 
+#            <a href="#traceback" class="jstoggle-text">[ hide ]</a>
+#          </p>
+#          <div id="traceback" class="jshide">{2}</div>
+#        </div> <!-- /single -->
+#        '''.format(excName, excArgs, excTb).split('\n')
+
+    def _fetch_record(self, session, recid):
+        session = self.session
+        db = self.database
+        queryFactory = self.queryFactory
+        qString = 'rec.identifier exact "{0}"'.format(recid)
+        q = queryFactory.get_query(session, qString)
+        rs = db.search(session, q)
+        try:
+            return rs[0].fetch_record(session)
+        except IndexError:
+            raise c3errors.FileDoesNotExistException(recid)
+
 
 def main():
     """Start up a simple app server to serve the SRU application."""
-    raise NotImplementedError('ead.py contains an Abstract Base Class')
+    raise NotImplementedError("cheshire3archives.apps.ead.base contains only"
+                              "an Abstract Base Class")
 
 
-application = EADWsgiApplication()
+session = Session()
+session.environment = "apache"
+serv = SimpleServer(session, os.path.join(cheshire3Root,
+                                          'configs',
+                                          'serverConfig.xml'))
+db = serv.get_object(session, 'db_ead')
+config = SafeConfigParser()
+application = EADWsgiApplication(session, db, config)
 
 
 if __name__ == "__main__":
---
+++ cheshire3archives/apps/ead/record.py
@@ -0,0 +1,155 @@
+"""EAD Record Resolver WSGI Application."""
+
+import sys
+import os
+import webbrowser
+import socket
+import mimetypes
+import cheshire3.exceptions as c3errors
+
+from cgi import FieldStorage
+from foresite import conneg
+from argparse import ArgumentParser
+
+from base import *
+
+
+class EADRecordWsgiApplication(EADWsgiApplication):
+    
+    def __init__(self, session, database, config):
+        super(EADRecordWsgiApplication, self).__init__(session,
+                                                       database,
+                                                       config)
+        self.mimetypeHash = mtHash = {'text/html': self.html,
+                                      'text/plain': self.text,
+                                      'application/xml': self.xml
+                                     }
+        # Fix for mimetypes module bug
+        mtHash.update({'text/xml': self.xml})
+        self.mimetypeList = conneg.parse(', '.join(mtHash.keys()))
+    
+    def __call__(self, environ, start_response):
+        # Method to make instances of this class callable
+        super(EADRecordWsgiApplication, self).__call__(environ, start_response)
+        self.environ = environ
+        path = environ.get('PATH_INFO', '').strip('/')
+        if path == "environ":
+            self.response_headers.append(('Content-Type', 'text/plain'))
+            start_response('200 OK', self.response_headers)
+            return [repr(i) + '\n' for i in environ.iteritems()]
+        self.globalReplacements.update({'SCRIPT': path,
+                                        '%SCRIPT%': path
+                                        }
+                                       )
+
+        # Parse request to determine record, Internet (MIME) Type etc.
+        recid, mimetype, encoding, args = self._parse_recSpec(path)
+        # Content negotiation if not specified by file extension
+        if mimetype is None:
+            mtrequested = environ.get('HTTP_ACCEPT', 'text/html')
+            mtc = conneg.parse(mtrequested)
+            mimetype = conneg.best(mtc, self.mimetypeList)
+            encoding = None
+        
+        # Fetch the Record
+        try:
+            rec = self._fetch_record(session, recid)
+        except c3errors.FileDoesNotExistException as e:
+            # Still could not be found - 404!
+            start_response("404 Not Found", self.response_headers)
+            return []
+        fn = self.mimetypeHash.get(str(mimetype), getattr(self, 'html'))
+        content = fn(rec, args)
+        print self.response_headers
+#        try:
+#            content = fn(rec, args)
+#        except:
+#            content = self._handle_error()
+#            start_response("500 Not Found", response_headers)
+#            return content
+        start_response("200 OK", self.response_headers)
+        return content
+
+    def _parse_recSpec(self, path_info):
+        recid = path_info
+        mType, encoding = mimetypes.guess_type(path_info)
+        if mType is not None:
+            # There is a filename extension to strip off
+            recid, ext = os.path.splitext(recid)
+        fields = FieldStorage(fp=self.environ['wsgi.input'], environ=self.environ)
+        # Normalize form
+        form = {}
+        for qp in fields.list:
+            if qp.value.isdigit():
+                form[qp.name] = int(qp.value)
+            else:
+                form[qp.name] = qp.value
+        return recid, mType, encoding, form
+
+    def html(self, rec, form):
+        self.response_headers.append(('Content-Type', 'text/html'))
+        raise NotImplementedError()
+
+    def xml(self, rec, form):
+        self.response_headers.append(('Content-Type', 'application/xml'))
+        raise NotImplementedError()
+
+    def text(self, rec, form):
+        self.response_headers.append(('Content-Type', 'text/plain'))
+        raise NotImplementedError()
+
+
+def main(argv=None):
+    """Start up a simple app server to serve the SRU application."""
+    global argparser, application
+    from wsgiref.simple_server import make_server
+    if argv is None:
+        args = argparser.parse_args()
+    else:
+        args = argparser.parse_args(argv)
+    httpd = make_server(args.hostname, args.port, application)
+    url = "http://{0}:{1}".format(args.hostname, args.port)
+    if args.browser:
+        webbrowser.open(url)
+        print ("Hopefully a new browser window/tab should have opened "
+               "displaying the application.")
+        print "If not, you should be able to access the application at:"
+    else:
+        print "You should be able to access the application at:"
+        
+    print url
+    return httpd.serve_forever()
+
+
+application = EADRecordWsgiApplication(session, db, config)
+
+
+# Set up argument parser
+argparser = ArgumentParser(description=__doc__.splitlines()[0])
+# Find default hostname
+try:
+    hostname = socket.gethostname()
+except:
+    hostname = 'localhost'
+
+argparser.add_argument('--hostname', type=str,
+                       action='store', dest='hostname',
+                       default=hostname, metavar='HOSTNAME',
+                       help=("name of host to listen on. default derived by "
+                             "inspection of local system"))
+argparser.add_argument('-p', '--port', type=int,
+                       action='store', dest='port',
+                       default=8008, metavar='PORT',
+                       help="number of port to listen on. default: 8008")
+argparser.add_argument('--no-browser',
+                       action='store_false', dest='browser',
+                       default=True,
+                       help=("don't open a browser window/tab containing the "
+                             "app. useful if you want to deploy the app for "
+                             "other users"
+                             )
+                       )
+
+
+if __name__ == "__main__":
+    sys.exit(main())
