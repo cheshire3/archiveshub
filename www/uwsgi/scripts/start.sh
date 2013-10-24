c9423fad1d56236be41c9f25b04109446e50e7a3
John Harrison <john.harrison@liv.ac.uk>
2013-10-24 11:44:51 +0100

    improve uwsgi emperor process management

--- www/uwsgi/scripts/start.sh
+++ www/uwsgi/scripts/start.sh
@@ -6,4 +6,4 @@ fi
 
 # Start uWSGI Emperor
 # https://uwsgi-docs.readthedocs.org/en/latest/Emperor.html
-uwsgi --emperor ~/archiveshub/www/uwsgi/apps --vassals-include ~/archiveshub/www/uwsgi/vassals.ini --emperor-pidfile ~/archiveshub/www/uwsgi/archiveshub.pid --daemonize ~/archiveshub/www/uwsgi/archiveshub.log --emperor-stats-server 127.0.0.1:8888
+uwsgi --emperor ~/archiveshub/www/uwsgi/apps --vassals-include ~/archiveshub/www/uwsgi/vassals.ini --emperor-pidfile ~/archiveshub/www/uwsgi/logs/archiveshub.pid --daemonize ~/archiveshub/www/uwsgi/logs/archiveshub.log --emperor-stats-server 127.0.0.1:8888
