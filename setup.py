0327fc0636022a945ad3f023012d5784325158b0
John Harrison <john.harrison@liv.ac.uk>
2014-01-21 12:26:45 +0000

    bumped release candidate number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from archiveshub.setuptools.commands import (develop,
                                              ) 
 
 _name = 'archiveshub'
-_version = '3.2.0c3'
+_version = '3.2.0c4'
 
 # Inspect to find current path
 setuppath = inspect.getfile(inspect.currentframe())
