7deb5841b3eca8ef7aab327bf1e5a1dca181ec3a
John Harrison <john.harrison@liv.ac.uk>
2014-04-25 17:34:17 +0100

    add WSGI wrapped Editor to demo server

---
+++ archiveshub/apps/editor/edit.py
@@ -0,0 +1,49 @@
+"""Archives Hub Editor Administration Application."""
+
+from __future__ import absolute_import
+
+import sys
+
+from mod_python_wsgi.wrap import BasicAuthModPythonWSGIApp
+
+from archiveshub.deploy.utils import WSGIAppArgumentParser
+from archiveshub.apps.editor.hubEditingHandler import (
+    handler,
+    authenhandler
+    )
+
+
+def main(argv=None):
+    """Start up a simple app server to serve the application."""
+    global argparser, application
+    from wsgiref.simple_server import make_server
+    if argv is None:
+        args = argparser.parse_args()
+    else:
+        args = argparser.parse_args(argv)
+    httpd = make_server(args.hostname, args.port, application)
+    url = "http://{0}:{1}".format(args.hostname, args.port)
+    if args.browser:
+        import webbrowser
+        webbrowser.open(url)
+        print ("Hopefully a new browser window/tab should have opened "
+               "displaying the application.")
+        print "If not, you should be able to access the application at:"
+    else:
+        print "You should be able to access the application at:"
+
+    print url
+    return httpd.serve_forever()
+
+
+application = BasicAuthModPythonWSGIApp(handler, authenhandler)
+
+
+# Set up argument parser
+argparser = WSGIAppArgumentParser(
+    conflict_handler='resolve',
+    description=__doc__.splitlines()[0]
+)
+
+if __name__ == '__main__':
+    sys.exit(main())
--- archiveshub/deploy/cherrypy_serve.py
+++ archiveshub/deploy/cherrypy_serve.py
@@ -28,6 +28,7 @@ from archiveshub.deploy.utils import WSGIAppArgumentParser
 from archiveshub.apps.ead.search import application as ead_search_app
 from archiveshub.apps.ead.record import application as ead_data_app
 from archiveshub.apps.editor.admin import application as admin_app
+from archiveshub.apps.editor.edit import application as edit_app
 
 def main(argv=None):
     """Start up a CherryPy server to serve the SRU application."""
@@ -56,6 +57,7 @@ def main(argv=None):
     cherrypy.tree.graft(ead_data_app, '/data')
     cherrypy.tree.graft(ead_search_app, '/search')
     cherrypy.tree.graft(admin_app, '/admin')
+    cherrypy.tree.graft(edit_app, '/edit')
     root_conf = {
         'tools.staticdir.on': True,
         'tools.staticdir.dir': expanduser('~/mercurial/archiveshub/htdocs'),
