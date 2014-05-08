a8f7a2efc016af46803ba6d6babdb683ca048542
John Harrison <john.harrison@liv.ac.uk>
2014-05-08 10:14:35 +0100

    preserve XML declaration (w/ encoding) at ingest
    Do not strip out the XML declaration during the ingest pipeline,
    as this prevents the XML parser from correctly detecting the
    character encoding used.

---
+++ archiveshub/preParser.py
@@ -0,0 +1,35 @@
+"""Archives Hub PreParser Implementations"""
+
+import re
+
+from cheshire3.document import StringDocument
+from cheshire3.preParser import SgmlPreParser
+
+
+class EADSgmlPreParser(SgmlPreParser):
+
+    def process_document(self, session, doc):
+        txt = doc.get_raw(session)
+        txt = txt.replace('\n', ' ')
+        txt = txt.replace('\r', ' ')
+        for x in range(9, 14):
+            txt = txt.replace('&#%d;' % (x), ' ')
+        txt = self.doctype_re.sub('', txt)
+        for e in self.entities.keys():
+            txt = txt.replace("&%s;" % (e), self.entities[e])
+        txt = self.amp_re.sub(self._loneAmpersand, txt)
+        txt = txt.replace('&<', '&amp;<')
+        txt = self.attr_re.sub(self._attributeFix, txt)
+        txt = self.elem_re.sub(self._lowerElement, txt)
+        for t in self.emptyTags:
+            empty_re = re.compile('<(%s( [^>/]+)?)[\s/]*>' % t)
+            txt = empty_re.sub(self._emptyElement, txt)
+        # Strip processing instructions
+        # Protect XML declaration if present
+        first_pi = self.pi_re.search(txt)
+        txt = self.pi_re.sub('', txt)
+        if first_pi and first_pi.group(0).startswith('<?xml '):
+           txt = first_pi.group(0) + '\n' + txt
+        return StringDocument(txt, self.id, doc.processHistory,
+                              mimeType=doc.mimeType, parent=doc.parent,
+                              filename=doc.filename)
