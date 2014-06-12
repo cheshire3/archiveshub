cheshire3/archiveshub

commit 414ef4cf2b5791a29f6bf8fd41fdb01d1ec604fb

strip all tags from <titleproper> in Editor
Mantis issue 846

Some tags that are permitted in <unittitle> are NOT permitted in
<titleproper>. Stript out all tags from <titleproper> to remove
risk of generating invalid EAD due to this.

archiveshub/apps/editor/hubEditingHandler.py
@@ -1536,8 +1536,8 @@ class HubEditingHandler(object):
             target = self._create_path(header,
                                        'filedesc/titlestmt/titleproper'
                                        )
-            self._add_text(target, form.get('did/unittitle', ''))
-
+            # Strip all tags from titleproper
+            target.text = re.sub('</?.+?>', '', form.get('did/unittitle', ''))
             # Add/delete sponsor
             sponsval = form.get('filedesc/titlestmt/sponsor', '').value
             if sponsval.strip() not in ['', ' ']:

commit 0fce350e3f28d2246c00995e56c824102ccfd958

fix Editor titleproper charset bug
Fix bug with `<titleproper>` encoding introduced in 414ef4cf

archiveshub/apps/editor/hubEditingHandler.py
@@ -1544,7 +1544,10 @@ class HubEditingHandler(object):
                                        'filedesc/titlestmt/titleproper'
                                        )
             # Strip all tags from titleproper
-            target.text = re.sub('</?.+?>', '', form.get('did/unittitle', ''))
+            self._add_text(
+                target,
+                re.sub('</?.+?>', '', form.get('did/unittitle', ''))
+            )
             # Add/delete sponsor
             sponsval = form.get('filedesc/titlestmt/sponsor', '').value
             if sponsval.strip() not in ['', ' ']: