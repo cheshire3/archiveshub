d747d707585d2d3aadb0de366d6b8ed29b51d9f5
Jerome Fuselier <jerome.fuselier@free.fr>
2015-02-09 21:47:27 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from archiveshub.setuptools.commands import (
 )
 
 _name = 'archiveshub'
-_version = '3.3.3'
+_version = '3.3.4'
 
 # Inspect to find current path
 setuppath = inspect.getfile(inspect.currentframe())
